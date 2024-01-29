6098ddb1eb8618bca038b2886f1e4b6fd8767851
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2024-01-29 23:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your JS code here.
+var img = document.getElementsByTagName('img');
+
+window.addEventListener(
+    'scroll',
+    debounce(
+		function (event) {
+			let currWindowScrollY = window.scrollY;
+			let currWindowInnerHeight = window.innerHeight;
+			let currMiddlePosition = currWindowScrollY + currWindowInnerHeight;
+			let loopLength = img.length;
+
+			for (let index = 0; index < loopLength; index++) {
+				let currImageOffset = img[index].offsetTop;
+				let currImageHeight = img[index].height;
+
+				if(currMiddlePosition >= currImageHeight + currImageOffset){
+					img[index].classList.add('active');
+				}
+			}
+	    }
+	)
+)
